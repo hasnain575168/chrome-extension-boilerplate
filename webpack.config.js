99afcf3718d76a0cffc94379231e16c37539e39f
unknown <hasnain575168@gmail.com>
2019-10-15 17:16:26 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = ({
+  mode
+}) => ({
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../styles/[name].css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
+        }
+      },
+      {
+        test: /.(s)?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hrm: mode
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  entry: {
+    popup: [
+      '@babel/polyfill',
+      './src/popup/index.js'
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'addon', 'scripts'),
+    filename: '[name].js'
+  },
+
+  mode
+
+});
